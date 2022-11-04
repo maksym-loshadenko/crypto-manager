52b34d6eb02feff124257adba50dc70b9986a27c
Maksym Loshadenko <maksym.loshadenko@gmail.com>
2022-11-04 23:15:18 +0200

    Feature #3: Implemented GetSpecificExchange method & RequestException
    Implemented a GetSpecificExchange method and added a custom RequestException.
    
    https://cryptingup.com/apidoc/#get-a-specific-exchange

--- CryptoManager/CryptoManager.Lib/Exchanges/CryptingUp/CryptingUpClient.cs
+++ CryptoManager/CryptoManager.Lib/Exchanges/CryptingUp/CryptingUpClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using CryptoManager.Lib.Exceptions.CryptingUp;
 using CryptoManager.Lib.Exchanges.CryptingUp.Enums;
 using CryptoManager.Lib.Exchanges.CryptingUp.Models;
 using Newtonsoft.Json.Linq;
@@ -13,43 +14,64 @@ namespace CryptoManager.Lib.Exchanges.CryptingUp
 
         public IEnumerable<Exchange> GetAllExchanges()
         {
-            var response =  MakeHttpRequest($"{BaseUrl}/exchanges").Result;
-
-            var responseObject = JObject.Parse(response);
-
-            return responseObject["exchanges"] switch
+            try
             {
-                null => throw new Exception($"{ErrorString} Exchanges list is null."),
-                JObject => throw new Exception($"{ErrorString} Exchanges list is not an array."),
-                _ => responseObject["exchanges"]?.ToObject<List<Exchange>>() ?? throw new Exception($"{ErrorString}")
-            };
+                var response = MakeHttpRequest($"{BaseUrl}/exchanges").Result;
+
+                var responseObject = JObject.Parse(response);
+
+                return responseObject["exchanges"] switch
+                {
+                    null => throw new Exception($"{ErrorString} Exchanges list is null."),
+                    JObject => throw new Exception($"{ErrorString} Exchanges list is not an array."),
+                    _ => responseObject["exchanges"]?.ToObject<List<Exchange>>() ??
+                         throw new Exception($"{ErrorString}")
+                };
+            }
+            catch (AggregateException e)
+            {
+                throw e.InnerException ?? new RequestException(ErrorString, "");
+            }
+        }
+
+        public Exchange GetSpecificExchange(string exchangeId)
+        {
+            try
+            {
+                var response = MakeHttpRequest($"{BaseUrl}/exchanges/{exchangeId}").Result;
+
+                var responseObject = JObject.Parse(response);
+
+                return responseObject["exchange"] switch
+                {
+                    null => throw new RequestException($"{ErrorString} Exchanges list is null.", response),
+                    _ => responseObject["exchange"]?.ToObject<Exchange>() ?? throw new RequestException($"{ErrorString}", response)
+                };
+            }
+            catch (AggregateException e)
+            {
+                throw e.InnerException ?? new RequestException(ErrorString, "");
+            }
         }
 
         public async Task<string> MakeHttpRequest(string url, Dictionary<string, string>? parameters = null)
         {
-            try
-            {
-                var result = WebClient.WebGetRequest(url, parameters).Result;
+            var result = WebClient.WebGetRequest(url, parameters).Result;
 
-                if (result.IsSuccessStatusCode)
-                {
-                    return await result.Content.ReadAsStringAsync();
-                }
-
-                throw new Exception(ParseStatusCode(result.StatusCode));
-            }
-            catch (Exception e)
+            if (result.IsSuccessStatusCode)
             {
-                throw new Exception(e.GetBaseException().Message);
+                return await result.Content.ReadAsStringAsync();
             }
+
+            throw new RequestException(ParseStatusCode(result.StatusCode), result.Content.ReadAsStringAsync().Result);
         }
 
         private static string ParseStatusCode(HttpStatusCode statusCode)
         {
-            if (!Enum.IsDefined(typeof(CryptingUpError), statusCode))
+            if (!Enum.IsDefined(typeof(CryptingUpError), (int)statusCode))
                 return "Unexpected error for CryptingUp.";
 
-            switch ((CryptingUpError)statusCode)
+            switch ((CryptingUpError)(int)statusCode)
             {
                 case CryptingUpError.BadRequest:
                     return "Bad request - Request is invalid";
