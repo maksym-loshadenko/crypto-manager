99c48e9ea3f78c5cb4b205e348ad33c45a0b6f6e
Maksym Loshadenko <maksym.loshadenko@gmail.com>
2022-11-05 13:05:41 +0200

    Updated Exchange class to not to serialize Quote after receiving data

--- CryptoManager/CryptoManager.Lib/Exchanges/CryptingUp/Models/Exchange.cs
+++ CryptoManager/CryptoManager.Lib/Exchanges/CryptingUp/Models/Exchange.cs
@@ -14,30 +14,34 @@ namespace CryptoManager.Lib.Exchanges.CryptingUp.Models
         [JsonProperty("website")]
         public string Website { get; set; } = "";
 
-        [JsonProperty("volume_24h")]
-        public double Volume24H { get; set; }
-
-        [JsonProperty("quote")] public JObject Quote { get; set; } = new();
-
         [JsonIgnore]
+        public JObject Quote { get; set; } = new();
+
+        [JsonProperty("quote")]
+        public JObject QuoteSetter
+        {
+            set => Quote = value;
+        }
+
+        [JsonProperty("volume_24h_cad")]
         public double? Volume24Cad => Quote["CAD"]?["volume_24h"]?.ToObject<double>();
 
-        [JsonIgnore]
+        [JsonProperty("volume_24h_usd")]
         public double? Volume24Usd => Quote["USD"]?["volume_24h"]?.ToObject<double>();
 
-        [JsonIgnore]
+        [JsonProperty("volume_24h_eur")]
         public double? Volume24Eur => Quote["EUR"]?["volume_24h"]?.ToObject<double>();
 
-        [JsonIgnore]
+        [JsonProperty("volume_24h_gbp")]
         public double? Volume24Gbp => Quote["GBP"]?["volume_24h"]?.ToObject<double>();
 
-        [JsonIgnore]
+        [JsonProperty("volume_24h_jpy")]
         public double? Volume24Jpy => Quote["JPY"]?["volume_24h"]?.ToObject<double>();
 
-        [JsonIgnore]
+        [JsonProperty("volume_24h_aud")]
         public double? Volume24Aud => Quote["AUD"]?["volume_24h"]?.ToObject<double>();
 
-        [JsonIgnore]
+        [JsonProperty("volume_24h_nzd")]
         public double? Volume24Nzd => Quote["NZD"]?["volume_24h"]?.ToObject<double>();
     }
 }
